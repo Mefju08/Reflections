d174eee320b66c7ae28085ca8bbb54d1adc7e7e9
Mateusz_Górny <59448497+Mefju08@users.noreply.github.com>
2025-06-25 12:33:33 +0200

    init

---
+++ terminal/Reflection.Console.App/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Reflections.Terminal.App.Models
+{
+    internal sealed class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private string _pesel = "123456789";
+
+        public void Greet() => Console.WriteLine($"Hi, {Name}!");
+        public void SetAge(int newAge) => Age = newAge;
+    }
+}
---
+++ terminal/Reflection.Console.App/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Reflections.Terminal.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
